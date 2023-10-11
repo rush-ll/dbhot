2c16e09bb4093a158faead45926bcd6a51e2ceed
lilin <2570530483@qq.com>
2023-10-11 21:56:23 +0800

    v1

---
+++ test.py
@@ -0,0 +1,9 @@
+score = int(input('请输入成绩：'))
+if 100 >= score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+else:
+    print('其他')
---
+++ xxx.py
@@ -0,0 +1,6 @@
+name = input('请输入用户名： ')
+pwd = input('请输入密码： ')
+if name == 'alex' and pwd == 'oldboy':
+    print("欢迎登录")
+else:
+    print("输入错误")
